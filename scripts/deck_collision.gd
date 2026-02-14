f3110ea89a681fd259af8e39eb62a862eaadeae0
Luís Cordeiro <luisfrpcordeiro@gmail.com>
2026-02-14 01:39:32 +0000

    First Commit
    Saved project on github

---
+++ scripts/deck_collision.gd
@@ -0,0 +1,17 @@
+extends CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _mouse_entered():
+	print(1)
+	
+func _mouse_exited():
+	print(1)
